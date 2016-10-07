af26b7a7e3a5096a19019625270971e34868ed11
Bram Pijper <brampijper@gmail.com>
2016-10-07 13:55:31 +0200

    Added service section with pictures and text in the most basic form

--- includes/main.js
+++ includes/main.js
@@ -7,58 +7,25 @@ $(document).ready(function() {
     var green = $(".color3")
     var speed = 2000;
 
-    function animate1 () {
+    function colorAnimate () {
     	$(".fruits").text("Mango")
     	$(yellow).animate({'background-color': '#FBDB0C'}, speed, function(){ 
     		$(".fruits").text("Forest Fruits");
     				$(yellow).animate({'background-color': '#6666cc'}, speed, function(){  //fruit of the forest
-    					$(".fruits").text("Lemon");
-    						$(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
-    							$(".fruits").text("Strawberry");
-    								$(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
+    					$(".fruits").text("Strawberry");
+    						$(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
+    							$(".fruits").text("Lemon");
+    								$(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
     									$(".fruits").text("Orange");
-    										$(yellow).animate({'background-color': '#FFA500'}, speed, function(){ //orange
-    											animate1();
+    										$(yellow).animate({'background-color': '#F7882F'}, speed, function(){ //orange
+    											colorAnimate();
     										});
     									}); 
     							}); 
     					}); 
     			});
     }
-    animate1(); 
-
-    // function animate2 () {
-
-    // 	$(red).animate({'background-color': '#ff4d4d'}, speed, function(){
-    // 		$(red).animate({'background-color': '#cc0000'}, speed, function(){
-    // 			$(red).animate({'background-color': '#ff7f7f'}, speed, function(){
-    // 				$(red).animate({'background-color': '#b20000'}, speed, function(){
-    // 					$(red).animate({'background-color': '#ff3232'}, speed, function(){
-    // 						animate2();
-    // 					});
-    // 				}); 
-    // 			}); 
-    // 		}); 
-    // 	});
-    // }
-    // animate2(); 
-
-    // function animate3 () {
-
-    // 	$(green).animate({'background-color': '#B0FC23'}, speed, function(){
-    // 		$(green).animate({'background-color': '#C3F42E'}, speed, function(){
-    // 			$(green).animate({'background-color': '#1e8133'}, speed, function(){
-    // 				$(green).animate({'background-color': '#45de07'}, speed, function(){
-    // 					$(green).animate({'background-color': '#73D77E'}, speed, function(){
-    // 						animate3();
-    // 					});
-    // 				}); 
-    // 			}); 
-    // 		}); 
-    // 	});
-    // }
-    // animate3(); 
-
+    colorAnimate(); 
 });
 
 function myMap() {
