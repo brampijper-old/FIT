915ce8c54254fb2c90df3939654dbf3e4cb65b75
Bram Pijper <brampijper@gmail.com>
2016-10-08 12:05:40 +0200

    Made the home page prettier -> updated contact info + google maps

--- includes/main.js
+++ includes/main.js
@@ -31,23 +31,24 @@ document.getElementById('abc').style.display = "none";
 }
 
 
+var myCenter = new google.maps.LatLng(41.878114, -87.629798);
 
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(52.343824, 4.850269), 
-    zoom: 14
+function initialize() {
+var mapProp = {
+center:myCenter,
+zoom:12,
+scrollwheel:false,
+draggable:false,
+mapTypeId:google.maps.MapTypeId.ROADMAP
+};
+
+var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+
+var marker = new google.maps.Marker({
+position:myCenter,
+});
+
+marker.setMap(map);
 }
-var map = new google.maps.Map(mapCanvas, mapOptions);
 
-addMaker(amsterdam, map); 
-}
-
-function addMaker (location, map) {
-	var marker = new google.maps.Marker({
-		position:location,
-		label: labels[labelIndex++ % labels.length],
-		maps: map
-	}) ; 
-
-}
+google.maps.event.addDomListener(window, 'load', initialize);
