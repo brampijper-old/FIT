edf80f60709c7b1cdc7f0e05764023f6d6c21356
Bram Pijper <brampijper@gmail.com>
2016-10-08 14:18:22 +0200

    fixed some bugs and typo's -> added falling ice cream cones when clicking on emergency button -> still need to make the images sharp and only run the function when the user clicks the button

--- includes/main.js
+++ includes/main.js
@@ -31,24 +31,119 @@ document.getElementById('abc').style.display = "none";
 }
 
 
-var myCenter = new google.maps.LatLng(41.878114, -87.629798);
 
-function initialize() {
-var mapProp = {
-center:myCenter,
-zoom:12,
-scrollwheel:false,
-draggable:false,
-mapTypeId:google.maps.MapTypeId.ROADMAP
-};
 
-var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-var marker = new google.maps.Marker({
-position:myCenter,
-});
 
-marker.setMap(map);
+// get a refrence to the canvas and its context
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+
+// newly spawned objects start at Y=25
+var spawnLineY = 0;
+
+// spawn a new object every 1500ms
+var spawnRate = 1500;
+
+// set how fast the objects will fall
+var spawnRateOfDescent = 1.0;
+
+// when was the last object spawned
+var lastSpawn = -1;
+
+// this array holds all spawned object
+var objects = [];
+
+// save the starting time (used to calc elapsed time)
+var startTime = Date.now();
+
+var img1 = new Image();
+img1.src = "pic/brownice.png";
+
+var img2 = new Image();
+img2.src = "pic/pinkice.png";
+
+var img3 = new Image();
+img3.src = "pic/blueice.png";
+
+// Our images array
+var images = [img1, img2, img3];
+
+// start animating
+window.onload=function(){
+animate();
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+
+
+console.log(images)
+
+function spawnRandomObject() {
+
+    // select a random type for this new object
+    var t;
+
+    // About Math.random()
+    // Math.random() generates a semi-random number
+    // between 0-1. So to randomly decide if the next object
+    // will be A or B, we say if the random# is 0-.49 we
+    // create A and if the random# is .50-1.00 we create B
+
+    if (Math.random() < 0.50) {
+        t = "red";
+    } else {
+        t = "blue";
+    }
+
+    // create the new object
+    var object = {
+        // set this objects type
+        type: t,
+        // set x randomly but at least 15px off the canvas edges
+        x: Math.random() * (canvas.width - 30) + 15,
+        // set y to start on the line where objects are spawned
+        y: spawnLineY,
+        // give random image
+        image: images[Math.floor(Math.random()*images.length)]
+    }
+    // add the new object to the objects[] array
+    objects.push(object);
+}
+
+
+
+function animate() {
+
+    // get the elapsed time
+    var time = Date.now();
+
+    // see if its time to spawn a new object
+    if (time > (lastSpawn + spawnRate)) {
+        lastSpawn = time;
+        spawnRandomObject();
+    }
+
+    // request another animation frame
+    requestAnimationFrame(animate);
+
+    // clear the canvas so all objects can be 
+    // redrawn in new positions
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    
+    // draw the line where new objects are spawned
+    ctx.beginPath();
+    ctx.moveTo(0, spawnLineY);
+    ctx.lineTo(canvas.width, spawnLineY);
+    ctx.stroke();
+    
+    // move each object down the canvas
+    for (var i = 0; i < objects.length; i++) {
+        var object = objects[i];
+        object.y += spawnRateOfDescent;
+        ctx.drawImage(object.image, object.x, object.y, 30, 30);
+    }
+
+}
+
+
+
