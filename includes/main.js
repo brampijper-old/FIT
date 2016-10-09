21b644c09738f53c732bd8eacd7ed73cd05faaaf
Bram Pijper <brampijper@gmail.com>
2016-10-09 16:00:37 +0200

    Fixed merge conflict by removing old code

--- includes/main.js
+++ includes/main.js
@@ -1,49 +1,29 @@
-$(document).ready(function() {
-    $("#truck").animate({marginLeft: "+=50%"}, speed);
-    // $("#truck").animate({marginLeft: "-=700"}, 1000)
+var yellow = $(".color1")
+var red = $(".color2")
+var green = $(".color3")
+var speed = 2000;
 
-    var yellow = $(".color1")
-    var red = $(".color2")
-    var green = $(".color3")
-    var speed = 2000;
+function colorAnimate () {
+ $(".fruits").text("Mango")
+ $(yellow).animate({'background-color': '#FBDB0C'}, speed, function(){ 
 
-    function colorAnimate () {
-    	$(".fruits").text("Mango")
-    	$(yellow).animate({'background-color': '#FBDB0C'}, speed, function(){ 
-    		$(".fruits").text("Forest Fruits");
-    				$(yellow).animate({'background-color': '#6666cc'}, speed, function(){  //fruit of the forest
-    					$(".fruits").text("Strawberry");
-    						$(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
-    							$(".fruits").text("Lemon");
-    								$(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
-    									$(".fruits").text("Orange");
-    										$(yellow).animate({'background-color': '#F7882F'}, speed, function(){ //orange
-    											colorAnimate();
-    										});
-    									}); 
-    							}); 
-    					}); 
-    			});
-    }
-    colorAnimate(); 
-});
+ $(".fruits").text("Forest Fruits");
+ $(yellow).animate({'background-color': '#6666cc'}, speed, function(){  //fruit of the forest
 
-function myMap() {
-  var mapCanvas = document.getElementById("map");
-  var mapOptions = {
-    center: new google.maps.LatLng(52.343824, 4.850269), 
-    zoom: 14
-  }
-  var map = new google.maps.Map(mapCanvas, mapOptions);
+ $(".fruits").text("Strawberry");
+ $(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
+                
+ $(".fruits").text("Lemon");
+ $(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
+                      
+ $(".fruits").text("Orange");
+ $(yellow).animate({'background-color': '#F7882F'}, speed, function(){ //orange
 
-  addMaker(amsterdam, map); 
+ colorAnimate();
+ });
+ }); 
+ }); 
+ }); 
+ });
 }
-
-function addMaker (location, map) {
-	var marker = new google.maps.Marker({
-		position:location,
-		label: labels[labelIndex++ % labels.length],
-		maps: map
-	}) ; 
-
-}
+colorAnimate(); 
