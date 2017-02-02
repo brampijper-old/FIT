c26c76e7aa2ad52fded2688eeff769ac67663f33
Giggleball <boldewijn.eli@gmail.com>
2017-02-02 20:34:49 +0100

    why is the carousel getting small before sliding over??

--- includes/main.js
+++ includes/main.js
@@ -17,36 +17,36 @@ $(document).ready(function() {
         $("#thirdLine").slideDown('slow');
     }, 6500)
 
-//flavor color changer banner
-    var yellow = $(".color1")
-    var red = $(".color2")
-    var green = $(".color3")
-    var speed = 2000;
+// //flavor color changer banner
+//     var yellow = $(".color1")
+//     var red = $(".color2")
+//     var green = $(".color3")
+//     var speed = 2000;
 
-    function colorAnimate () {
-         $(".fruits").text("Mango")
-         $(yellow).animate({'background-color': '#FBDB0C'}, speed, function(){ 
+//     function colorAnimate () {
+//          $(".fruits").text("Mango")
+//          $(yellow).animate({'background-color': '#FBDB0C'}, speed, function(){ 
 
-         $(".fruits").text("Forest Fruits");
-         $(yellow).animate({'background-color': '#6666cc'}, speed, function(){  //fruit of the forest
+//          $(".fruits").text("Forest Fruits");
+//          $(yellow).animate({'background-color': '#6666cc'}, speed, function(){  //fruit of the forest
 
-         $(".fruits").text("Strawberry");
-         $(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
+//          $(".fruits").text("Strawberry");
+//          $(yellow).animate({'background-color': '#ff4d4d'}, speed, function(){ //strawberry
                         
-         $(".fruits").text("Lemon");
-         $(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
+//          $(".fruits").text("Lemon");
+//          $(yellow).animate({'background-color': '#B0FC23'}, speed, function(){ //lemon
                               
-         $(".fruits").text("Orange");
-         $(yellow).animate({'background-color': '#F7882F'}, speed, function(){ //orange
+//          $(".fruits").text("Orange");
+//          $(yellow).animate({'background-color': '#F7882F'}, speed, function(){ //orange
 
-         colorAnimate();
-         });
-         }); 
-         }); 
-         }); 
-         });
-    }
-    colorAnimate(); 
+//          colorAnimate();
+//          });
+//          }); 
+//          }); 
+//          }); 
+//          });
+//     }
+//     colorAnimate(); 
 
 });
 
